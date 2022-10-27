8679ccde6159cf95bacb2a9cad13e6e98073b351
Evan <jimmy7525@naver.com>
2022-10-27 12:41:57 +0900

    수정

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -751,11 +751,12 @@ public enum ToastPosition {
     case top
     case center
     case bottom
-    
+    case bottomPlusHeight
+
     fileprivate func centerPoint(forToast toast: UIView, inSuperview superview: UIView) -> CGPoint {
         let topPadding: CGFloat = ToastManager.shared.style.verticalPadding + superview.csSafeAreaInsets.top
         let bottomPadding: CGFloat = ToastManager.shared.style.verticalPadding + superview.csSafeAreaInsets.bottom
-        
+
         switch self {
         case .top:
             return CGPoint(x: superview.bounds.size.width / 2.0, y: (toast.frame.size.height / 2.0) + topPadding)
@@ -763,6 +764,8 @@ public enum ToastPosition {
             return CGPoint(x: superview.bounds.size.width / 2.0, y: superview.bounds.size.height / 2.0)
         case .bottom:
             return CGPoint(x: superview.bounds.size.width / 2.0, y: (superview.bounds.size.height - (toast.frame.size.height / 2.0)) - bottomPadding)
+        case .bottomPlusHeight:
+          return CGPoint(x: superview.bounds.size.width / 2.0, y: (superview.bounds.size.height - (toast.frame.size.height / 2.0)) - bottomPadding - 30)
         }
     }
 }
